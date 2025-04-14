01dd411afa550e3c65bed5526bfe1cc608a65a37
mkt883 <mtd01211@gmail.com>
2025-04-14 11:22:07 +0100

    trying to center pop up video

--- popups.js
+++ popups.js
@@ -34,7 +34,9 @@ function ktok(){
     
     popUp.innerHTML =  `
     <h1>Kaunos Tomb of Kings:</h1>
+    <div class="centerImg">
         <video controls loop autoplay poster="images/thumbnails/ktok-thumbnail.png" aria-label="A video showcasing the Kaunos Tomb of Kings in Turkey recreated in Unreal Engine 5" src="https://media.githubusercontent.com/media/mkt883/portfolio/main/images/posts/vp/MA2807_KaunosTombOfKings.mp4" width="800px"></video>
+    </div>
         <br><br>
         <strong>Using:</strong> <em>Unreal Engine, DaVinci Resolve</em><br><br>
         <div class="top-bot-borders">
@@ -113,8 +115,8 @@ function fairlady(){
     popUp.innerHTML =  `
     <h1>Fairlady:</h1>
         <div class="centerImg">
-            <img style="max-height: 400px;" alt="A t-shirt graphic print showing a woman leaning on a nissan fairlady 350z in a garage" src="images/posts/graphics/fairlady_watermarked.jpg">
-            <img style="max-height: 400px;" alt="A t-shirt graphic print showing a woman leaning on a nissan fairlady 350z in a garage" src="images/posts/graphics/fairlady_watermarked_2.jpg">
+            <img style="max-height: 400px; min-height: 200px;" alt="A t-shirt graphic print showing a woman leaning on a nissan fairlady 350z in a garage" src="images/posts/graphics/fairlady_watermarked.jpg">
+            <img style="max-height: 400px; min-height: 200px" alt="A t-shirt graphic print showing a woman leaning on a nissan fairlady 350z in a garage" src="images/posts/graphics/fairlady_watermarked_2.jpg">
         </div>
         <br>
         <strong>Using:</strong> <em>Photoshop/Photopea</em><br><br>
